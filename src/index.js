7015c1cfda2c92921e04cdc1ef3dc6a7fe34e2a5
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-12-01 13:05:12 -0800

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reactComponent = <App />;
+const reactRoot = document.getElementById('reactRoot');
+
+ReactDOM.render(reactComponent, reactRoot);
+
 registerServiceWorker();
